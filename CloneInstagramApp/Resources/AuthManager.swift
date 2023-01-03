b24fed304bbe1a4d9113998d217c0e1aa8c0c13f
LinhNH <linhnh@hachinet.com>
2023-01-03 16:20:45 +0700

    new commit pod,firebase

---
+++ CloneInstagramApp/Resources/AuthManager.swift
@@ -0,0 +1,14 @@
+
+import FirebaseAuth
+
+public class AuthManager {
+    static let shared = AuthManager()
+    
+    public func registerNewUser(username : String, email : String, password: String){
+        
+    }
+    
+    public func loginUser(username: String?, email: String?, passowrd: String?){
+        
+    }
+}
---
+++ CloneInstagramApp/Resources/DatabaseManager.swift
@@ -0,0 +1,14 @@
+//
+//  DatabaseManager.swift
+//  CloneInstagramApp
+//
+//  Created by HOANG LINH on 03/01/2023.
+//
+
+import FirebaseDatabase
+
+public class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    
+}
