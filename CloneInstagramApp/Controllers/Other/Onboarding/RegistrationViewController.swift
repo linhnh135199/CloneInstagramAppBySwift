862063e87a4ebceba89387822fbd304c847046d3
LinhNH <linhnh@hachinet.com>
2023-01-04 16:52:44 +0700

    fix ui

--- CloneInstagramApp/Controllers/Other/Onboarding/RegistrationViewController.swift
+++ CloneInstagramApp/Controllers/Other/Onboarding/RegistrationViewController.swift
@@ -70,12 +70,31 @@ class RegistrationViewController: UIViewController {
         button.setTitleColor(.white, for: .normal)
         return button
     }()
+    
+    private let haveAccountButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("Already have an account? Sign In", for: .normal)
+        return button
+    }()
+    
+    private let headerView: UIView = {
+        let header = UIView()
+        header.clipsToBounds = true
+        //add background img
+        let backgroundImageView = UIImageView(image: UIImage(named: "gradient"))
+        header.addSubview(backgroundImageView)
+        return header
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         registerButton.addTarget(self,
                                  action: #selector(didTapRegister),
                                  for: .touchUpInside)
+        haveAccountButton.addTarget(self
+                              , action: #selector(didTapHaveAccount)
+                              , for: .touchUpInside)
         usernameField.delegate = self
         emailField.delegate = self
         passwordField.delegate = self
@@ -88,13 +107,19 @@ class RegistrationViewController: UIViewController {
         view.addSubview(emailField)
         view.addSubview(passwordField)
         view.addSubview(registerButton)
+        view.addSubview(haveAccountButton)
+        view.addSubview(headerView)
     }
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+        headerView.frame = CGRect(x: 0
+                                  , y: 0.0
+                                  , width: view.width
+                                  , height: view.height/4.0)
         usernameField.frame = CGRect(x: 20,
-                                     y: view.safeAreaInsets.top + 100,
+                                     y: view.safeAreaInsets.top + 150,
                                      width: view.width - 40,
                                      height: 52)
         emailField.frame = CGRect(x: 20,
@@ -109,6 +134,32 @@ class RegistrationViewController: UIViewController {
                                      y: passwordField.bottom + 10,
                                      width: view.width - 40,
                                      height: 52)
+        haveAccountButton.frame = CGRect(x: 10,
+                                     y: view.height - view.safeAreaInsets.bottom - 50,
+                                     width: view.width - 20,
+                                     height: 50)
+        
+        configureHeaderView()
+    }
+    private func configureHeaderView(){
+        guard headerView.subviews.count == 1 else { return }
+        
+        guard let backgroundView = headerView.subviews.first else { return }
+        
+        backgroundView.frame = headerView.bounds
+        
+        //add logo
+        let imageView = UIImageView(image: UIImage(named: "text"))
+        headerView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: headerView.width/4.0,
+                                 y: view.safeAreaInsets.top,
+                                 width: headerView.width/2.0,
+                                 height: headerView.height - view.safeAreaInsets.top)
+        }
+    @objc private func didTapHaveAccount(){
+        let vc = LoginViewController()
+        present(vc, animated: true)
     }
     
     @objc private func didTapRegister(){
