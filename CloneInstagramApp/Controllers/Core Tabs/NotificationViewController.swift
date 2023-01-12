e8f54ab03eb515d54160fd1482af1626d1b8ef8a
LinhNH <linhnh@hachinet.com>
2023-01-12 18:12:01 +0700

    new code notification

--- CloneInstagramApp/Controllers/Core Tabs/NotificationViewController.swift
+++ CloneInstagramApp/Controllers/Core Tabs/NotificationViewController.swift
@@ -11,15 +11,31 @@ class NotificationViewController: UIViewController,UITableViewDelegate, UITableV
     
     private let tableView: UITableView = {
         let tableView = UITableView()
-        tableView.register(UITableViewCell.self,
-                           forCellReuseIdentifier: "cell")
+        tableView.isHidden = false
+        tableView.register(NotificationLikeEventTableViewCell.self,
+                           forCellReuseIdentifier: NotificationLikeEventTableViewCell.identifier)
+        tableView.register(NotificationFollowEventTableViewCell.self,
+                           forCellReuseIdentifier: NotificationFollowEventTableViewCell.identifier)
+
         return tableView
     }()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.tintColor = .label
+        return spinner
+    }()
+    
+    private lazy var noNotificationsView = NoNofiticationsView()
+    //lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Notifications"
         view.backgroundColor = .systemBackground
+        view.addSubview(spinner)
+        //spinner.startAnimating()
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
@@ -27,7 +43,24 @@ class NotificationViewController: UIViewController,UITableViewDelegate, UITableV
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
+        spinner.frame = CGRect(x: 0,
+                               y: 0,
+                               width: 100,
+                               height: 100)
+        spinner.center = view.center
+        addNotificationView()
     }
+    
+    private func addNotificationView(){
+        tableView.isHidden = true
+        view.addSubview(tableView)
+        noNotificationsView.frame = CGRect(x: 0,
+                                           y: 0,
+                                           width: view.width/2,
+                                           height: view.width/4)
+        noNotificationsView.center = view.center
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
         
---
+++ CloneInstagramApp/Views/Cells/Notifications/NotificationLikeEventTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  NotificationLikeEventTableViewCell.swift
+//  CloneInstagramApp
+//
+//  Created by HOANG LINH on 10/01/2023.
+//
+
+import UIKit
+protocol NotificationLikeEventTableViewCellDelegate: AnyObject {
+    func didTapRelatedPostButton(model: String)
+}
+class NotificationLikeEventTableViewCell: UITableViewCell {
+    
+    static let identifier = "NotificationLikeEventTableViewCell"
+    weak var delegate: NotificationLikeEventTableViewCellDelegate?
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let label:UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let postButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.clipsToBounds = true
+        addSubviews()
+    }
+    private func addSubviews(){
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(label)
+        contentView.addSubview(postButton)
+    }
+    
+    public func configure(with model: String){
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        postButton.setBackgroundImage(nil, for: .normal)
+        label.text = nil
+        profileImageView.image = nil
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
